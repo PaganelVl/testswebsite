bf3a23170e097e285eaa3485b388c9dab081b0d0
Paganel <vladimir.ru.dino@gmail.com>
2023-04-02 09:57:08 +0300

    PEP 8

--- mysite/apps/accounts/urls.py
+++ mysite/apps/accounts/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
+
 urlpatterns = [
 	path('login/', views.MySiteLoginView.as_view(), name='MySiteLoginView'),
 	path('register/', views.RegisterUserView.as_view(), name='RegisterUserView'),
