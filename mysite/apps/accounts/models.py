4d855119bfd7a69ae57713f64be631cb9ffe1b62
Paganel <vladimir.ru.dino@gmail.com>
2023-04-05 09:56:37 +0300

    PEP 8

--- mysite/apps/accounts/models.py
+++ mysite/apps/accounts/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 
-class User (models.Model):
-	first_name = models.CharField(max_length = 100, verbose_name = "Имя")
-	last_name = models.CharField(max_length = 100, verbose_name = "Фамилия")
-	username = models.CharField(max_length = 100, verbose_name = "Имя пользователя")
-	password = models.CharField(max_length = 100, verbose_name = "Пароль")
+class User(models.Model):
+	first_name = models.CharField(max_length=100, verbose_name="Имя")
+	last_name = models.CharField(max_length=100, verbose_name="Фамилия")
+	username = models.CharField(max_length=100, verbose_name="Имя пользователя")
+	password = models.CharField(max_length=100, verbose_name="Пароль")
 	
 	class Meta:
-		verbose_name ='Пользователь'
+		verbose_name = 'Пользователь'
 		verbose_name_plural = 'Пользователи'
 
 	def __str__(self):
